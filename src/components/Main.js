eba157485d5eb387cebee6f508c0f08b0ce1bdb2
Almerindo Paixão <almerindopaixao@gmail.com>
2020-07-23 16:23:14 -0300

    oi

---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Form from './Form';
+import './Main.css';
+
+export default function Main() {
+    return (
+        <>
+            <h1 className="main-title">Main</h1>
+            <Form />
+        </>
+    );
+}
