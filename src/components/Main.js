b28e7267356d333401a8236690bce6e1cdf6f239
Almerindo Paixão <almerindopaixao@gmail.com>
2020-07-24 00:41:19 -0300

    final update

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,27 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 import Form from './Form';
 import './Main.css';
+import showDate from '../utils/showDate';
 
 export default function Main() {
-    return (
-        <>
-            <h1 className="main-title">Main</h1>
-            <Form />
-        </>
-    );
+  const [date, setDate] = useState('');
+  const [daysMonthsYears, setDaysMonthsYears] = useState({
+    days: 0,
+    months: 0,
+    years: 0,
+  });
+
+  return (
+    <>
+      <h1 className="main-title">Datas</h1>
+      <Form
+        date={date}
+        setDate={setDate}
+        daysMonthsYears={daysMonthsYears}
+        setDaysMonthsYears={setDaysMonthsYears}
+      />
+
+      <p className="date">{showDate(date, daysMonthsYears)}</p>
+    </>
+  );
 }
